ac111d214b20aba0447808da738057e09100851b
Nickolay Konstantinov <konst007@tut.by>
2021-01-31 18:53:03 +0300

    add app

---
+++ app/src/main/java/com/konst007/myapplication/MyFragment.kt
@@ -0,0 +1,30 @@
+package com.konst007.myapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class MyFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.view_my_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            val changeableText = view.findViewById<TextView>(R.id.text_view)
+            changeableText.translationX = 200f
+            changeableText.setTextColor(Color.CYAN)
+        }
+    }
+}
